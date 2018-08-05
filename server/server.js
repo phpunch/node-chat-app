db5e0f9133301ce55ca8a9d145a4eb7962ed37e7
Nuttapon Vittayaprechapon <nutacp_ryho@hotmail.com>
2018-08-05 18:27:36 +0700

    Add new message and create message event

--- server/server.js
+++ server/server.js
@@ -14,6 +14,18 @@ app.use(express.static(publicPath))
 io.on('connection', (socket) => {
     console.log('New user connected')
 
+    socket.emit('newMessage', {
+        from: 'John',
+        text: 'dgfsdfdsf',
+        createAt: 123456
+    })
+
+    socket.on('createMessage', (newMessage) => {
+        console.log('create message', newMessage)
+    })
+
+
+
     socket.on('disconnect', () => {
         console.log('User was disconnected')
     })
